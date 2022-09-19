7b95b50e654ea0db83b14942760a469fb8c03452
chiachun wu <a0283137@gmail.com>
2022-09-19 17:23:32 +0800

    pic&JQ

---
+++ bar/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link href="http://fonts.cdnfonts.com/css/melinda" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/ukij-chiwer-kesme" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+
+<script src="./jqyery3.6.1.js"></script>
+</body>
+</html>
