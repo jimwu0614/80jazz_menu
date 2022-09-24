93b8f2fca7b3fc48193d88439435ccb565412d08
chiachun wu <a0283137@gmail.com>
2022-09-25 01:52:39 +0800

    whiskey

--- bar/whiskey.php
+++ bar/whiskey.php
@@ -1,5 +1,7 @@
-<div class="whiskey ">
-    <div class="single"></div>
+<div class="whiskey">
+    <div class="whiskeytitle">
+        <span class="single">Single</span><span class="blended">Blended</span><span class="bourbon">Bourbon</span>
+    </div>
     <table>
         <tr>
             <td></td>
