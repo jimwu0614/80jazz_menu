df3e9d2c4997f53d2d63390f1c84e5a56d2bc86a
chiachun wu <a0283137@gmail.com>
2022-09-24 12:54:31 +0800

    navbar&main

--- bar/index.php
+++ bar/index.php
@@ -6,38 +6,67 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link href="http://fonts.cdnfonts.com/css/melinda" rel="stylesheet">
     <link href="http://fonts.cdnfonts.com/css/ukij-chiwer-kesme" rel="stylesheet">
-    <link rel="stylesheet" href="./css/style.css">
+    <link href="http://fonts.cdnfonts.com/css/pixelade" rel="stylesheet">
+    <link rel="stylesheet" href="./css/nav.css">
+    <link rel="stylesheet" href="./css/neon.css">
 </head>
 
 <body>
-    <nav role="navigation">
-        <div id="menuToggle">
-            <input type="checkbox" />
-            <span></span>
-            <span></span>
-            <span></span>
-            <ul id="menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Info</a></li>
-                <li><a href="#">Contact</a></li>
+    <div id="wrapper">
+        <div class="overlay"></div>
+
+        <!-- Sidebar -->
+        <nav class="navbar navbar-inverse fixed-top" id="sidebar-wrapper" role="navigation">
+            <ul class="nav sidebar-nav">
+                <div class="sidebar-header">
+                    <div class="sidebar-brand">
+                        <a href="#">Brand</a>
+                    </div>
+                </div>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#events">Events</a></li>
+                <li><a href="#team">Team</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#followme">Follow me</a></li>
             </ul>
-        </div>
-    </nav>
-    <div class="container wiskey">
-        <div class="row">
-            <div class="col">
-                <div class="con">
-                    <div style="height: 300vh; width:100vw;">內容內容內容內容內容內容內容內容</div>
+        </nav>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <button type="button" class="hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
+                <span class="hamb-top"></span>
+                <span class="hamb-middle"></span>
+                <span class="hamb-bottom"></span>
+            </button>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <?php
+							$do=$_GET['do']??'main';
+                            $file='./'.$do.".php";
+                            if(file_exists($file)){
+                                include $file;
+                            }else{
+                                include "./main.php";
+                            }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        <!-- /#page-content-wrapper -->
 
-    <script src="./jqyery3.6.1.js"></script>
+    </div>
+    <!-- /#wrapper -->
+    <script src="./js/jqyery3.6.1.js"></script>
+    <script src="./js/js.js"></script>
 </body>
 
 </html>
---
+++ bar/main.php
@@ -0,0 +1,6 @@
+<div class="centered body">
+    <div>
+        <h1 class="neon-effect">I love NEON</h1>
+        <h1 class="neon-effect">Awesome</h1>
+    </div>
+</div>
