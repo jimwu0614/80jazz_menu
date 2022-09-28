24125e1f94e84cdc419e6227e82a0da39b0aee00
jimwu0614 <a0283137@gmail.com>
2022-09-28 14:22:55 +0800

    主頁footer

--- bar/front/main.php
+++ bar/front/main.php
@@ -4,7 +4,10 @@
             <h1 class="neon-effect">80 Jazz</h1>
             <h1 class="neon-effect">Lounge Bar</h1>
         </div>
-<br>
+    <br>
     <a href="#" class="neon-button" data-toggle="offcanvas">Menu</a>
     </div>
 </div>
+<footer class="footer">
+    <a href="back.php">Copyright © 2022 80-jazz. All rights reserved. Design by Jim Wu</a>
+</footer>
