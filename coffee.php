f8eaff39a0aad7ef4f30fb177e139e2a925c91a7
chiachun wu <a0283137@gmail.com>
2022-10-03 23:43:04 +0800

    coffee BG

--- coffee.php
+++ coffee.php
@@ -8,66 +8,38 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">    
     <link href="http://fonts.cdnfonts.com/css/melinda" rel="stylesheet">
     <link href="http://fonts.cdnfonts.com/css/ukij-chiwer-kesme" rel="stylesheet">
-    <link rel="stylesheet" href="./css/coffee.css">
     <link rel="stylesheet" href="./css/bgpic.css">
+    <link rel="stylesheet" href="./css/coffee.css">
 
 </head>
 <body>
     <div class="coffee"></div>
     <div class="container">
         <div class="row mt-4">
-            <div class="col-3"></div>
-            <div class="col mt-5">
-                <span class="cn">義式濃縮</span>&nbsp;&nbsp;&nbsp;<span class="en">Espresso</span><span class="price">.......$XXX</span> <br>
-                <span class="cn">美式</span>&nbsp;&nbsp;&nbsp;<span class="en">Americano </span><span class="price">.......$XXX</span> <br>
-                <span class="cn">拿鐵</span>&nbsp;&nbsp;&nbsp;<span class="en"> Caffè latte</span><span class="price">.......$XXX</span> <br>
-                <span class="cn">榛果拿鐵</span>&nbsp;&nbsp;&nbsp;<span class="en"> Hazelnuts latte</span><span class="price">.......$XXX</span> <br>
-                <span class="cn">焦糖瑪奇朵</span>&nbsp;&nbsp;&nbsp;<span class="en"> caramel macchiato</span><span class="price">.......$XXX</span> <br>
+
+            <div class="col-12 mt-5">
+                <span class="cn">義式濃縮</span><span class="en">Espresso</span><span class="price">.......$XXX</span> <br>
+                <span class="cn">美式</span><span class="en">Americano </span><span class="price">.......$XXX</span> <br>
+                <span class="cn">拿鐵</span><span class="en"> Caffè latte</span><span class="price">.......$XXX</span> <br>
+                <span class="cn">榛果拿鐵</span><span class="en"> Hazelnuts latte</span><span class="price">.......$XXX</span> <br>
+                <span class="cn">焦糖瑪奇朵</span><span class="en"> caramel macchiato</span><span class="price">.......$XXX</span> 
             </div>
             <div class="col-4  mt-5">
-                <br><br>
-                &nbsp;&nbsp;<span class="cn">水果茶</span>&nbsp;&nbsp;&nbsp;<span class="en">Fruit Tea </span><span class="price">.......$XXX</span> <br>
-                &nbsp;&nbsp;<span class="cn">花草茶</span>&nbsp;&nbsp;&nbsp;<span class="en">Flour Tea </span><span class="price">.......$XXX</span> <br>
+                <span class="cn">水果茶</span><span class="en">Fruit Tea </span><span class="price">.......$XXX</span> <br>
+                <span class="cn">花草茶</span><span class="en">Flour Tea </span><span class="price">.......$XXX</span> <br>
             </div>
         </div>
-        <div class="row mt-3"><br><br></div>
+
         <div class="row mt-3">
             <div class="col-4"></div>
             <div class="col">
-                <span class="cn">愛爾蘭咖啡</span>&nbsp;&nbsp;&nbsp;<span class="en">Irish coffee</span><span class="price">.......$XXX</span> <br>
-                <span class="cn">咖啡馬丁尼</span>&nbsp;&nbsp;&nbsp;<span class="en">Expresso Martini</span><span class="price">.......$XXX</span> <br>
-                <span class="cn">咖啡琴湯尼</span>&nbsp;&nbsp;&nbsp;<span class="en">Turbo Gin & Tonic</span><span class="price">.......$XXX</span> <br>
-                <span class="cn">漂浮咖啡波本</span>&nbsp;&nbsp;&nbsp;<span class="en">Coffee Bourbon Float</span><span class="price">.......$XXX</span> <br>
+                <span class="cn">愛爾蘭咖啡</span><span class="en">Irish coffee</span><span class="price">.......$XXX</span> <br>
+                <span class="cn">咖啡馬丁尼</span><span class="en">Expresso Martini</span><span class="price">.......$XXX</span> <br>
+                <span class="cn">咖啡琴湯尼</span><span class="en">Turbo Gin & Tonic</span><span class="price">.......$XXX</span> <br>
+                <span class="cn">漂浮咖啡波本</span><span class="en">Coffee Bourbon Float</span><span class="price">.......$XXX</span> <br>
             </div>
             
         </div>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" style="">
-    Open modal
-</button>
-<!-- The Modal -->
-<div class="modal fade" id="myModal">
-  <div class="modal-dialog modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">Modal Heading</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-      </div>
-
-      <!-- Modal body -->
-      <div class="modal-body">
-        Modal body..
-      </div>
-
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-      </div>
-
-    </div>
-  </div>
-</div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 
