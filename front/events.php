8f00824d2c9d534ff35290e111cf51bf02136be8
jimwu0614 <a0283137@gmail.com>
2022-11-08 01:58:11 +0800

    活動頁架構

--- front/events.php
+++ front/events.php
@@ -1,6 +1,17 @@
 <div class="centered body">
-    <div class="pic">
-        <img src="./img/rules.jpg" alt="">
+    <div class="centered body">
+        <div class="box">
+            <div class="liquortitle">
+                <span class="type active">Tips</span>
+                <span class="type">Opening</span>
+                <span class="type">Cocktails</span>
+            </div>
+            <div class="blur">
+                <div class="pic">
+                    <img src="./img/rules.jpg" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
