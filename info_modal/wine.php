d2ae8ce3d3dcf270696dbae0edac3b3553e2df76
jimwu0614 <a0283137@gmail.com>
2022-12-07 02:03:47 +0800

    紅酒modal

--- info_modal/wine.php
+++ info_modal/wine.php
@@ -10,26 +10,18 @@ $data = $Wine->find($_POST['itemid']);
 
 <!-- Modal body -->
 <div class="modal-body">
-    <!-- <div class="w60"><img src="" alt=""></div> -->
-    <div class="w90 ma linehigh">
-        <img class="itemModelPic" src="./img/menupic/wine/<?=$data['image']?>.jpg" alt="">
+    <div class="flex_nowrap">
+        <div class="w50">
+            <img class="itemModelPic" src="./img/menupic/wine/<?=$data['image']?>.jpg" alt="">
+        </div>
+        <div class="modalLine">
+        </div>
+        <div class="flexcol">
+            <div class="modalItem">產區:<br><?=$data['from']?></div>
+            <br>
+            <div class="modalItem">介紹:<br><?=$data['intro']?></div>
+        </div>
     </div>
-    <!-- <div class="w80 ma flex_nowrap jc mt-3">
-        <div class="flexcol">
-            <div><p class="m-0">酒感/</p><span>Strong</span></div>
-            <div><?=$data['feel']?></div>
-        </div>
-        <div class="flexcol">
-            <div><p class="m-0">酸度/</p><span>Acidity</span></div>
-            <div><?=$data['acidity']?></div>
-        </div>
-        <div class="flexcol">
-            <div><p class="m-0">甜度/</p><span>Sweetness</span></div>
-            <div><?=$data['sweet']?></div>
-        </div>
-    </div> -->
-
-
 </div>
 
 <!-- Modal footer -->
